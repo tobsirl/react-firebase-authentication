b9312884a2ae4839172c82d3ede5a6bc42d79c85
Paul Tobin <tobster111@gmail.com>
2018-12-14 10:47:59 +0000

    Added routes + navigation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from '../Navigation';
 
 const App = () => (
-  <div>
+  <Router>
     <h1>App</h1>
-  </div>
+  </Router>
 );
 
 export default App;
